396d4578c138535e73316ef538808336bb8aa941
Bilyana Ilieva <bilyanailieva@192.168.100.10>
2025-07-20 17:21:45 +0300

    test site ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import solid from "@astrojs/solid-js";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  },
+  site: 'https://astro-frontend-masters-bilyanailieva.netlify.app',
+  integrations: [react({
+    include: ['**/*.react.tsx', '**/*.react.jsx']
+  }), solid({
+    include: ['**/*.solid.tsx', '**/*.solid.jsx']
+  })],
+  adapter: netlify()
+});
